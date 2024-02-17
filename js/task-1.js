97ccf9f2db6219d5ce38802471a98142dad70eae
Tatianadardik44 <149703862+Tatianadardik44@users.noreply.github.com>
2024-02-17 13:45:06 +0100

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,18 @@
+// function slugify(title) {
+//     let normalizeTitle = title.toLowerCase();
+//     let slug = normalizeTitle.split(" ");
+//     return slug.join("-");
+    
+  
+// }
+
+
+function slugify(title) {
+    title.toLowerCase();
+    let slug = title.split(" ");
+    return slug.join("-");
+}
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
